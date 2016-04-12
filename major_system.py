841b7646f586fe4a8922cb385a45a96d88a8435f
Vincent Fiorentini <vincent@panatechcomputer.com>
2016-04-12 01:44:21 -0700

    Created ContextEncoder base class and NgramContextEncoder derived class for incorporating previous words as context to an ngram model.

--- major_system.py
+++ major_system.py
@@ -2,9 +2,8 @@
 # By Vincent Fiorentini and Megan Shao, (c) 2016.
 
 from pronouncer import Pronouncer
-from number_encoder import NumberEncoder
-from number_encoder import RandomGreedyEncoder
-from number_encoder import UnigramGreedyEncoder
+from number_encoder import (NumberEncoder, RandomGreedyEncoder, UnigramGreedyEncoder,
+                            NgramContextEncoder)
 from ngram_evaluator import NgramEvaluator
 
 def main():
@@ -13,22 +12,22 @@ def main():
     '''
 
     # Demonstrate Pronouncer
-    print('=== Demonstrating the Pronouncer class ===')
+    # print('=== Demonstrating the Pronouncer class ===')
     pronouncer = Pronouncer()
-    number_encoder = NumberEncoder(pronouncer = pronouncer)
-    words = ['apple', 'ABUSE', 'Rainbow']
-    for word in words:
-        print('-- Pronunciation and decoding for \'{0}\' --'.format(word))
-        print('\'{0}\' has pronunciations (with stress): {1}'.format(word,
-            str(pronouncer.pronounce(word, strip_stress = False))))
-        print('\'{0}\' has pronunciations (no stress): {1}'.format(word,
-            str(pronouncer.pronounce(word))))
-        print('\'{0}\' can be pronounced: {1}'.format(word,
-            str(' '.join(pronouncer.pronounce(word)[0]))))
-        print('\'{0}\' is an encoding for the number {1}'.format(word,
-            number_encoder.decode_word(word)))
-    print('The sentence \'{0}\' is an encoding for the number {1}'.format(' '.join(words),
-        number_encoder.decode_words(words)))
+    # number_encoder = NumberEncoder(pronouncer = pronouncer)
+    # words = ['apple', 'ABUSE', 'Rainbow']
+    # for word in words:
+    #     print('-- Pronunciation and decoding for \'{0}\' --'.format(word))
+    #     print('\'{0}\' has pronunciations (with stress): {1}'.format(word,
+    #         str(pronouncer.pronounce(word, strip_stress = False))))
+    #     print('\'{0}\' has pronunciations (no stress): {1}'.format(word,
+    #         str(pronouncer.pronounce(word))))
+    #     print('\'{0}\' can be pronounced: {1}'.format(word,
+    #         str(' '.join(pronouncer.pronounce(word)[0]))))
+    #     print('\'{0}\' is an encoding for the number {1}'.format(word,
+    #         number_encoder.decode_word(word)))
+    # print('The sentence \'{0}\' is an encoding for the number {1}'.format(' '.join(words),
+    #     number_encoder.decode_words(words)))
 
     # Demonstrate NgramEvaluator
     print('\n=== Demonstrating the NgramEvaluator class ===')
@@ -61,13 +60,30 @@ def main():
     # Demonstrate UnigramGreedyEncoder
     print('\n=== Demonstrating the UnigramGreedyEncoder class ===')
     unigram_greedy_encoder = UnigramGreedyEncoder(pronouncer = pronouncer, max_word_length = 2)
-    numbers = ['123', '123', '451', '451', '12345', '0123456789',
+    numbers = ['123', '451', '12345', '0123456789',
                '31415926535897932384626433832795028841971693993751']
     for max_word_length in [1, 2, 3, 10]:
         print('-- Encoding with max_word_length {0} --'.format(max_word_length))
         for number in numbers:
             encoding = unigram_greedy_encoder.encode_number(number, max_word_length=max_word_length)
             decoding = unigram_greedy_encoder.decode_words(encoding)
+            print('The number \'{0}\' can be encoded as \'{1}\' (which decodes to \'{2}\').'.
+                format(number, encoding, decoding))
+            score = evaluator.score(encoding)
+            print('The score for this encoding is {0}.'.format(score))
+
+    # Demonstrate NgramContextEncoder
+    print('\n=== Demonstrating the NgramContextEncoder class ===')
+    for ngram_n in [1, 2, 3, 4]:
+        print('\n-- Encoding with n-gram model n = {0} --'.format(ngram_n))
+        ngram_context_encoder = NgramContextEncoder(pronouncer = pronouncer, max_word_length = 5,
+            n = ngram_n, alpha = 0.1, select_most_likely = True)
+        evaluator = NgramEvaluator(ngram_n)
+        numbers = ['123', '451', '12345', '0123456789',
+                   '31415926535897932384626433832795028841971693993751']
+        for number in numbers:
+            encoding = ngram_context_encoder.encode_number(number)
+            decoding = ngram_context_encoder.decode_words(encoding)
             print('The number \'{0}\' can be encoded as \'{1}\' (which decodes to \'{2}\').'.
                 format(number, encoding, decoding))
             score = evaluator.score(encoding)
--- number_encoder.py
+++ number_encoder.py
@@ -7,6 +7,8 @@ from random import sample # for RandomGreedyEncoder
 from itertools import product # for RandomGreedyEncoder
 from nltk.corpus import brown # for RandomGreedyEncoder
 from collections import Counter # for RandomGreedyEncoder
+from numpy.random import choice # for NgramContextEncoder
+from math import exp # for NgramContextEncoder
 
 class NumberEncoder(object):
     '''
@@ -31,6 +33,23 @@ class NumberEncoder(object):
         '''
         # this method is what really distinguishes one encoder from another
         raise NotImplementedError()
+
+    def _encode_number_chunk(self, number_chunk):
+        '''
+        Helper method, such as for encode_number(), that returns a list of all single-word
+        encodings for the given number_chunk.
+        '''
+        # enumerate all possible sequences of phonemes for number_chunk
+        possible_phonemes = []
+        for digit in number_chunk:
+            possible_phonemes += [tuple([phoneme for phoneme in self.phoneme_to_digit_dict
+                                         if digit == self.phoneme_to_digit_dict[phoneme]])]
+        # check every possible phoneme sequence in self.phonemes_to_words_dict and add every word
+        possible_encodings = []
+        for phoneme_sequence in product(*possible_phonemes):
+            if phoneme_sequence in self.phonemes_to_words_dict:
+                possible_encodings += self.phonemes_to_words_dict[phoneme_sequence]
+        return possible_encodings
 
     def decode_words(self, words):
         '''
@@ -92,6 +111,34 @@ class NumberEncoder(object):
             for phoneme in phonemes:
                 phoneme_to_digit_dict[phoneme] = digit
         return phoneme_to_digit_dict
+
+    def _get_phonemes_to_words_dict(self, vocabulary = None):
+        '''
+        Helper method that returns a dictionary that maps every sequence (tuple) of phonemes (that
+        are included in self.phoneme_to_digit_dict) to a list of all words in
+        self.pronouncer.pronunciation_dictionary that can be pronounced as that sequence.
+        Only includes keys with values (i.e., only includes sequences that actually appear).
+        '''
+        phonemes_to_words_dict = dict()
+        for word, pronunciations in self.pronouncer.pronunciation_dictionary.items():
+            pronunciations = self.pronouncer.strip_phonemes_stress(pronunciations)
+            # if this word is not in the vocabulary, do not include this word
+            if vocabulary != None and word.lower() not in vocabulary:
+                continue
+            # if any of the pronunciations yield different decodings, do not include this word
+            possible_decodings = self._decode_phonemes_list(pronunciations)
+            if not all(decoding == possible_decodings[0] for decoding in possible_decodings):
+                continue
+            # include the phoneme sequence for every pronunciation
+            for pronunciation in pronunciations:
+                included_phonemes = tuple([phoneme for phoneme in pronunciation
+                                           if phoneme in self.phoneme_to_digit_dict])
+                # if we've already included a word for this phoneme sequence, extend its list
+                if included_phonemes in phonemes_to_words_dict:
+                    phonemes_to_words_dict[included_phonemes] += [word]
+                else:
+                    phonemes_to_words_dict[included_phonemes] = [word]
+        return phonemes_to_words_dict
 
 
 class GreedyEncoder(NumberEncoder):
@@ -157,51 +204,6 @@ class GreedyEncoder(NumberEncoder):
                 return None
         return encodings
         # return ' '.join(encodings)
-
-    def _encode_number_chunk(self, number_chunk):
-        '''
-        Helper method for encode_number() that returns a list of all single-word encodings for the
-        given number_chunk.
-        '''
-        # enumerate all possible sequences of phonemes for number_chunk
-        possible_phonemes = []
-        for digit in number_chunk:
-            possible_phonemes += [tuple([phoneme for phoneme in self.phoneme_to_digit_dict
-                                         if digit == self.phoneme_to_digit_dict[phoneme]])]
-        # check every possible phoneme sequence in self.phonemes_to_words_dict and add every word
-        possible_encodings = []
-        for phoneme_sequence in product(*possible_phonemes):
-            if phoneme_sequence in self.phonemes_to_words_dict:
-                possible_encodings += self.phonemes_to_words_dict[phoneme_sequence]
-        return possible_encodings
-
-    def _get_phonemes_to_words_dict(self, vocabulary = None):
-        '''
-        Returns a dictionary that maps every sequence (tuple) of phonemes (that are included in
-        self.phoneme_to_digit_dict) to a list of all words in
-        self.pronouncer.pronunciation_dictionary that can be pronounced as that sequence.
-        Only includes keys with values (i.e., only includes sequences that actually appear).
-        '''
-        phonemes_to_words_dict = dict()
-        for word, pronunciations in self.pronouncer.pronunciation_dictionary.items():
-            pronunciations = self.pronouncer.strip_phonemes_stress(pronunciations)
-            # if this word is not in the vocabulary, do not include this word
-            if vocabulary != None and word.lower() not in vocabulary:
-                continue
-            # if any of the pronunciations yield different decodings, do not include this word
-            possible_decodings = self._decode_phonemes_list(pronunciations)
-            if not all(decoding == possible_decodings[0] for decoding in possible_decodings):
-                continue
-            # include the phoneme sequence for every pronunciation
-            for pronunciation in pronunciations:
-                included_phonemes = tuple([phoneme for phoneme in pronunciation
-                                           if phoneme in self.phoneme_to_digit_dict])
-                # if we've already included a word for this phoneme sequence, extend its list
-                if included_phonemes in phonemes_to_words_dict:
-                    phonemes_to_words_dict[included_phonemes] += [word]
-                else:
-                    phonemes_to_words_dict[included_phonemes] = [word]
-        return phonemes_to_words_dict
 
     def _get_max_digits_per_word(self):
         '''
@@ -284,3 +286,117 @@ class UnigramGreedyEncoder(GreedyEncoder):
                 max_prob = prob
                 max_prob_encoding = encoding
         return max_prob_encoding
+
+class ContextEncoder(NumberEncoder):
+    '''
+    ContextEncoder is the base class for any encoder that uses the previously chosen words as
+    context and somehow picks from the set of all possible words (possibly of limited word length).
+    '''
+
+    def __init__(self, pronouncer = Pronouncer(), phoneme_to_digit_dict = None,
+        max_word_length = None, context_length = 2):
+        '''
+        Initializes the ContextEncoder. The encoder will consider the context of at most
+        context_length previous words to choose the best subsequent word.
+        '''
+        super(ContextEncoder, self).__init__(pronouncer = pronouncer,
+            phoneme_to_digit_dict = phoneme_to_digit_dict)
+        # with the default encoding, the maximum number of digits in any word is 19
+        max_digits_per_word = 19
+        # if we were given an unusual max_word_length, set it to max_digits_per_word
+        if max_word_length == None or max_word_length < 1 or max_word_length > max_digits_per_word:
+            max_word_length = max_digits_per_word
+        self.max_word_length = max_word_length
+
+        max_reasonable_context = 5 # this limit is arbitrary; 5 was chosen with n-grams in mind
+        # if we were given an unusual context_length, set it to max_reasonable_context
+        if context_length == None or context_length < 0 or context_length > max_reasonable_context:
+            context_length = max_reasonable_context
+        self.context_length = context_length
+
+        # to aid encode_number(), we set up a mapping from phoneme sequences to words
+        self.phonemes_to_words_dict = self._get_phonemes_to_words_dict()
+
+    def _select_encoding(self, previous_words, encodings):
+        '''
+        Selects one encoding from the given list of possible encodings, given a tuple of
+        previous_words strings as context.
+        '''
+        # this method is what really distinguishes one context encoder from another
+        raise NotImplementedError()
+
+    def encode_number(self, number, max_word_length = None, context_length = None):
+        '''
+        Encodes the given number (string of digits) as a series of words. Considers all possible
+        digit chunks up to max_word_length (if given, otherwise self.max_word_length). Based on the
+        previous context_length (if given, otherwise self.context_length) words, the encoder
+        selects a word via self._select_encoding().
+        '''
+        # if not given max_word_length, use class default
+        if max_word_length == None:
+            max_word_length = self.max_word_length
+
+        # if not given context_length, use class default
+        if context_length == None:
+            context_length = self.context_length
+
+        encoded_index = 0 # the last index of number we've encoded, inclusive
+        encodings = []
+        while encoded_index < len(number):
+            # for all possible chunks starting at this position, find all possible encodings
+            chunk_encodings = set()
+            for chunk_length in range(1, max_word_length + 1):
+                number_chunk = number[encoded_index : encoded_index + chunk_length]
+                chunk_encodings |= set(self._encode_number_chunk(number_chunk))
+            # check that there are possible encodings to choose from
+            if len(chunk_encodings) == 0:
+                number_chunk = number[encoded_index : encoded_index + max_word_length]
+                print('Cannot find encoding for number chunk \'{0}\''.format(number_chunk))
+                return None
+            # select the best encoding from chunk_encodings
+            context = tuple(encodings[len(encodings) - context_length : len(encodings)])
+            chunk_encoding = self._select_encoding(context, list(chunk_encodings))
+            encodings += [chunk_encoding]
+            # increment encoded_index based on the chosen chunk_encoding
+            encoded_index += len(self.decode_word(chunk_encoding))
+
+        return encodings
+
+class NgramContextEncoder(ContextEncoder):
+    '''
+    NgramContextEncoder is a ContextEncoder that selects the most common encoding according to an
+    n-gram model.
+    '''
+
+    def __init__(self, pronouncer = Pronouncer(), phoneme_to_digit_dict = None,
+        max_word_length = None, n = 3, alpha = 0.1, select_most_likely = True):
+        '''
+        Initializes the NgramContextEncoder. The encoder will consider the context of at most
+        (n - 1) previous words and choose the subsequent word with highest n-gram probability
+        if select_most_likely is True (otherwise, will sample weighted by probabilities).
+        '''
+        super(NgramContextEncoder, self).__init__(pronouncer = pronouncer,
+            phoneme_to_digit_dict = phoneme_to_digit_dict, max_word_length = max_word_length,
+            context_length = n - 1)
+        self.ngram = NgramModel(n, alpha = alpha)
+        self.select_most_likely = select_most_likely
+
+    def _select_encoding(self, previous_words, encodings):
+        '''
+        Selects the most common encoding according to n-gram probabilities.
+        '''
+        if self.select_most_likely:
+            max_prob = -float('inf')
+            max_prob_encoding = None
+            for encoding in encodings:
+                prob = self.ngram.prob(previous_words, encoding)
+                if prob > max_prob:
+                    max_prob = prob
+                    max_prob_encoding = encoding
+            return max_prob_encoding
+        else:
+            probabilities = [exp(self.ngram.prob(previous_words, encoding))
+                             for encoding in encodings]
+            probability_sum = sum(probabilities)
+            probabilities_norm = [probability / probability_sum for probability in probabilities]
+            return choice(encodings, p = probabilities_norm)
